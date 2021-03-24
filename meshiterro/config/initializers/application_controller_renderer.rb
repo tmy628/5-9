9774e131e4177af0c6ad05fac804d1e0a6e2d451
tmy628 <tmy6h1r@gmail.com>
2021-03-24 07:56:39 +0000

    [Start] 5-9

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'bff44451af1ee84b1033a26696b96c0429439ce71458d3b25a4bf7b13de9556fd6fbf72026d117620a7db9d040e75caf76048ad9c4d044f9b7ce5925e3c40a7b'
